862deef66fa5c49e2344a0b479e4896206d46f45
James Russell <james.russell@tmwunlimited.com>
2020-10-20 17:21:18 +0100

    Modfiy fundDetails class to meet acceptance criteria
    Potential issue with the MarketCode but out of time

--- Api/DataFiles/FundDetails.cs
+++ Api/DataFiles/FundDetails.cs
@@ -1,19 +1,32 @@
+using Newtonsoft.Json;
 using System;
 
 namespace Api.DataFiles
 {
     public class FundDetails
     {
-        public Guid Id { get; set; }
+        private decimal _currentUnitPrice;
+
+        private Guid Id { get; set; }
 
         public bool Active { get; set; }
 
-        public decimal CurrentUnitPrice { get; set; }
+        public decimal CurrentUnitPrice {
+            get
+            {
+                return decimal.Round(_currentUnitPrice, 2);
+            }
+            set
+            {
+                _currentUnitPrice = value;
+            }
+        }
 
         public string FundManager { get; set; }
 
         public string Name { get; set; }
 
-        public string MarketCode { get; set; }
+        [JsonProperty("marketCode")]
+        public string Code { get; set; }
     }
 }
